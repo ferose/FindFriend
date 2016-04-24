3254de702012252ec8a75d6184c3f986d755803f
Ferose Babu <ferose2@gmail.com>
2016-04-24 12:39:20 -0400

    Initial Commit

---
+++ FindFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindFriend
+//
+//  Created by Ferose Babu on 4/24/16.
+//  Copyright © 2016 WWI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindFriend
+//
+//  Created by Ferose Babu on 4/24/16.
+//  Copyright © 2016 WWI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
